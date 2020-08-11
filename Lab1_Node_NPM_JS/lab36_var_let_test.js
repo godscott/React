b11f46fd19a5693a585832b2e122281345440d3f
cyhsieh <cyhsieh@chtti302>
2020-08-11 14:25:17 +0800

    var_let

---
+++ Lab1_Node_NPM_JS/lab36_var_let_test.js
@@ -0,0 +1,19 @@
+var a = 100
+var b = 200
+if (a === 100) {
+    console.log("inside scope, b=",b)
+    //console.log("inside scope, a=",a)
+    let a = 50
+    var b = 400
+    console.log(a, b)
+}
+
+function xyz() {
+    console.log("inside xyz scope, b=",b) //undefined
+    //console.log("inside scope, a=",a)
+    let a = 55
+    var b = 444
+    console.log(a, b)
+}
+console.log(a, b)
+xyz()
