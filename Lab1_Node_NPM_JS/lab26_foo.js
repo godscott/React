882f7ed45bc7d8d6c5cf63c004f8031bc544cda4
cyhsieh <cyhsieh@chtti302>
2020-08-11 11:33:02 +0800

    include file

---
+++ Lab1_Node_NPM_JS/lab26_foo.js
@@ -0,0 +1,7 @@
+module.exports = {
+    ooo:function (data){
+        console.log("[lab26_foo]:[ooo]")
+        console.log(data)
+    },
+    status:'OK!'
+}
---
+++ Lab1_Node_NPM_JS/lab26_main.js
@@ -0,0 +1,5 @@
+var lab26_qoo = require('./lab26_qoo') //呼叫其他檔案的物件
+var lab26_foo = require('./lab26_foo')
+lab26_qoo.qoo("HiHiHi")
+lab26_foo.ooo("hoho")
+console.log(lab26_foo.status)
