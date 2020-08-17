a95686183338b1fe0ada2d51e688badb92e95136
cyhsieh <cyhsieh@chtti302>
2020-08-17 09:46:12 +0800

    buffer

---
+++ Lab1_Node_NPM_JS/lab51_buffer_from.js
@@ -0,0 +1,28 @@
+//Bufer編碼方式
+const string1 = "Hello"
+var buffer0 = new Buffer.from(string1)
+var buffer1 = new Buffer.from(string1, "ascii")
+var buffer2 = new Buffer.from(string1, "utf8")
+var buffer3 = new Buffer.from(string1, "utf16le")
+var buffer4 = new Buffer.from(string1, "ucs2")
+console.log("buffer0="+buffer0.toString('base64'))
+console.log("buffer0="+buffer0.toString('hex'))
+console.log("buffer1="+buffer1.toString('hex'))
+console.log("buffer2="+buffer2.toString('hex'))
+console.log("buffer3="+buffer3.toString('hex'))
+console.log("buffer4="+buffer4.toString('hex'))
+
+const string2 = "中文"
+var buffer00 = new Buffer.from(string2)
+var buffer10 = new Buffer.from(string2, "ascii")
+var buffer20 = new Buffer.from(string2, "utf8")
+var buffer30 = new Buffer.from(string2, "utf16le")
+var buffer40 = new Buffer.from(string2, "ucs2")
+//var buffer5 = new Buffer.from(string1, "big5")
+console.log("[base64]buffer00="+buffer00.toString('base64'))
+console.log("[hex-default]buffer00="+buffer00.toString('hex'))
+console.log("[hex-ascii]buffer10="+buffer10.toString('hex'))
+console.log("[hex-utf8]buffer20="+buffer20.toString('hex'))
+console.log("[hex-utf16]buffer30="+buffer30.toString('hex'))
+console.log("[hex-ucs2]buffer40="+buffer40.toString('hex'))
+//console.log("[hex-ms950]buffer5="+buffer5.toString('hex'))
