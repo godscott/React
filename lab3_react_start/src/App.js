d83cdd46468fbb52258f0c6c1f88fd219f818d70
cyhsieh <cyhsieh@chtti302>
2020-08-18 10:04:02 +0800

    react introduction

--- lab3_react_start/src/App.js
+++ lab3_react_start/src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'; //副檔名.js可以不用寫出來
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( //回傳只能有一個物件，如只有一個root的概念
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    // <div className="App">
+    //   <h1>Hello world</h1>
+    //   <h2>React Programming</h2>
+    // </div>
+
+    // React.createElement('div', null, <h1>Hello world</h1>) //OK
+    // React.createElement('div', null, <h1>Hello world</h1><h2>React Programming</h2>) //error
+    React.createElement('div', {className:'App'}, //置中
+        React.createElement('h1', null, "Hello world"),
+        React.createElement('h2', null, "React Programming"),
+        React.createElement('h3', null, '2020-Aug-18')) //新增多個元件的方式
   );
 }
 
