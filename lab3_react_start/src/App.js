a2f4b39a392590c1f0a68cac0262bce3dca833d1
cyhsieh <cyhsieh@chtti302>
2020-08-18 14:09:17 +0800

    convert App.js to class component

--- lab3_react_start/src/App.js
+++ lab3_react_start/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'; //副檔名.js可以不用寫出來
+import React, { Component } from 'react'; //副檔名.js可以不用寫出來
 // import logo from './logo.svg';
 import './App.css';
 import Dashboard1 from './components/Dashboard1';
@@ -7,50 +7,96 @@ import Person from './components/Person';
 import Pet from './components/Pet';
 import Counter from './components/Counter';
 
-function App() {
-  return ( //回傳只能有一個物件，如只有一個root的概念
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Mark", age: 43 },
+      { name: "James", age: 38 },
+      { name: "Tim", age: 33 },
+      { name: "Mary", age: 28 },
+      { name: "abby", age: 34 },
+      { name: "Kevin", age: 50 }
 
-    // <div className="App">
-    //   <h1>Hello world</h1>
-    //   <h2>React Programming</h2>
-    // </div>
+    ]
+  }
 
-    // React.createElement('div', null, <h1>Hello world</h1>) //OK
-    // React.createElement('div', null, <h1>Hello world</h1><h2>React Programming</h2>) //error
-    // React.createElement('div', {className:'App'}, //置中
-    //     React.createElement('h1', null, "Hello world"),
-    //     React.createElement('h2', null, "React Programming"),
-    //     React.createElement('h3', null, '2020-Aug-18')) //新增多個元件的方式
-
-    <div className="App">
-      <Counter step="2"/>
-      <Dashboard1/>
-      <Dashboard2/>
-      <Person name="Mark" age="43"/>
-      <Pet name="king" species="cat" />
-      <Person name="James" age="38">Team Leader</Person> 
-      <Person name="Tim" age="33"/>
-      <Person name="Mary" age="28"/>
-      <Person name="Abby" age="34"/>
-      <Pet />
-    </div>
-  );
+  changeNameHandler = () => {
+    console.log("change button clicked")
+    this.setState({
+      persons: [{ name: "X man", age: 45 },
+      { name: "Captain America", age: 35 },
+      { name: "Iron man", age: 42 },
+      { name: "Thor", age: 200 },
+      { name: "Groot", age: 5 },
+      { name: "Hawk", age: 40 }
+      ]
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Counter step="2" />
+        <Dashboard1 />
+        <Dashboard2 />
+        <button onClick={this.changeNameHandler}>Change</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Pet name="king" species="cat" />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >Team Leader</Person>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+        <Person name={this.state.persons[4].name} age={this.state.persons[4].age} />
+        <Person name={this.state.persons[5].name} age={this.state.persons[5].age} />
+        <Pet />
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return ( //回傳只能有一個物件，如只有一個root的概念
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Edit <code>src/App.js</code> and save to reload.
+//     //     </p>
+//     //     <a
+//     //       className="App-link"
+//     //       href="https://reactjs.org"
+//     //       target="_blank"
+//     //       rel="noopener noreferrer"
+//     //     >
+//     //       Learn React
+//     //     </a>
+//     //   </header>
+//     // </div>
+
+//     // <div className="App">
+//     //   <h1>Hello world</h1>
+//     //   <h2>React Programming</h2>
+//     // </div>
+
+//     // React.createElement('div', null, <h1>Hello world</h1>) //OK
+//     // React.createElement('div', null, <h1>Hello world</h1><h2>React Programming</h2>) //error
+//     // React.createElement('div', {className:'App'}, //置中
+//     //     React.createElement('h1', null, "Hello world"),
+//     //     React.createElement('h2', null, "React Programming"),
+//     //     React.createElement('h3', null, '2020-Aug-18')) //新增多個元件的方式
+
+//     <div className="App">
+//       <Counter step="2"/>
+//       <Dashboard1/>
+//       <Dashboard2/>
+//       <Person name="Mark" age="43"/>
+//       <Pet name="king" species="cat" />
+//       <Person name="James" age="38">Team Leader</Person> 
+//       <Person name="Tim" age="33"/>
+//       <Person name="Mary" age="28"/>
+//       <Person name="Abby" age="34"/>
+//       <Pet />
+//     </div>
+//   );
+// }
 
 export default App;
