57171012f9c1e2796cd5210aa9017ab113062dee
cyhsieh <cyhsieh@chtti302>
2020-08-24 14:36:43 +0800

    Implement CRUD Repository for JPA access

--- Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/model/Project.java
+++ Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/model/Project.java
@@ -3,17 +3,34 @@ package com.chtti.fullstack.demo.Backend1.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
 @Data //會自動生成Getter, Setter, toString()....
 @NoArgsConstructor
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String projectName;
     private String projectIdentifier;
     private String description;
     private Date startDate;
     private Date endDate;
+    private Date createAt;
+    private Date updateAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updateAt = new Date();
+    }
 
     public Project(String projectName, String projectIdentifier) {
         this.projectName = projectName;
