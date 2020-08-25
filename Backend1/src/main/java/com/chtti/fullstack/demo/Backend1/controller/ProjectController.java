3b8865ee59a250ee3e8ce4a76a1592415c16cc2e
cyhsieh <cyhsieh@chtti302>
2020-08-25 14:37:58 +0800

    add cross origin resource sharing for ProjectController

--- Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/controller/ProjectController.java
+++ Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/controller/ProjectController.java
@@ -15,6 +15,7 @@ import javax.validation.Valid;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/project")
 public class ProjectController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ProjectController.class.getSimpleName());
