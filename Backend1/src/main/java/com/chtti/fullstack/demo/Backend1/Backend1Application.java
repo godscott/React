94cb7c12acddb3e23faa9acea9e90fa529d5e820
cyhsieh <cyhsieh@chtti302>
2020-08-31 16:47:44 +0800

    implement ehcache

--- Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/Backend1Application.java
+++ Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/Backend1Application.java
@@ -2,7 +2,9 @@ package com.chtti.fullstack.demo.Backend1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class Backend1Application {
 
--- Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/controller/ProjectController.java
+++ Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/controller/ProjectController.java
@@ -36,7 +36,7 @@ public class ProjectController {
 
     @GetMapping("/all")
     public Iterable<Project> getAllProjects() {
-        return projectService.findAllProjects();
+        return projectService.findAllProjects(true);
     }
 
     @GetMapping("/{projectId}")
