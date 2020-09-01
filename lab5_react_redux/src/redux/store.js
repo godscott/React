1e606bcd46a0b0d44f0ffbe90863608560a781e0
cyhsieh <cyhsieh@chtti302>
2020-09-01 17:22:45 +0800

    applyMiddleware

--- lab5_react_redux/src/redux/store.js
+++ lab5_react_redux/src/redux/store.js
@@ -1,6 +1,6 @@
-import {createStore} from "redux"
+import {createStore, applyMiddleware} from "redux"
 import {composeWithDevTools} from 'redux-devtools-extension'
 import rootReducer from "./rootReducer"
-
-const store = createStore(rootReducer,composeWithDevTools())
+import logger from 'redux-logger'
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger)))
 export default store
